1497ce60cb3d1b026f4c84803f23c7b6694a055e
maitree shakya <maitree.shakya@gmail.com>
2018-03-23 12:30:05 +1100

    Login completed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google"> Login with Google  </a>
+          </li>
+        );
+      default:
+        return <li>
+          <a href='/api/logout'> Logout </a>
+        </li>;
+    }
+  }
   render() {
+    //console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">DSA</a>
+          <Link
+          to = { this.props.user ? '/surveys' : '/' }
+          className="left brand-logo">
+          DSA
+          </Link>
           <ul className="right">
-            <li>
-              <a href="/auth/google"> Login with Google  </a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -17,4 +37,7 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { auth: state.auth }
+}
+export default connect(mapStateToProps)(Header);
