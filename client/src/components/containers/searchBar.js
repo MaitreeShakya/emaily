5f258071d70b53a109f79c49f923d8b0fccf06e2
maitree shakya <maitree.shakya@gmail.com>
2018-04-03 16:51:55 +1000

    fixing form submission bug

--- client/src/components/containers/searchBar.js
+++ client/src/components/containers/searchBar.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 
 export default class searchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit= this.onFormSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+    this.setState({ term: event.target.value });
+  }
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-        <form className="input-group">
-          <input/>
-          <span className="input-group-btn">
-            <button type ="submit" className="btn btn-secondary">Search</button>
-          </span>
-          </form>
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Search"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Search</button>
+        </span>
+      </form>
     )
   }
 }
