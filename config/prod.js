dff9be8bb69be05bd8b787cce248d7559e47db22
maitree shakya <maitree.shakya@gmail.com>
2018-06-29 03:20:32 +1000

    testing dev credentials in heroku

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,8 @@ module.exports = {
   mongoURI: process.env.MONGODB_URI,
   cookieKey: process.env.COOKIE_KEY,
   googleRedirectURI: process.env.GOOGLE_REDIRECT_URI,
-  accessKeyId: 'AKIAISE7HY5NNPEYW3LA',
-  secretAccessKey: '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'
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID
+  // accessKeyId: 'AKIAISE7HY5NNPEYW3LA',
+  // secretAccessKey: '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'
 };
