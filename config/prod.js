e3f5b6ba8752b99139974a08afde79bb8709b4f7
maitree shakya <maitree.shakya@gmail.com>
2018-06-29 02:10:53 +1000

    Added credentials for upload

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,10 @@
 //prod.js - production keys
 module.exports = {
-   googleClientID: process.env.GOOGLE_CLIENT_ID,
-   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-   mongoURI: process.env.MONGODB_URI,
-   cookieKey: process.env.COOKIE_KEY,
-   googleRedirectURI: process.env.GOOGLE_REDIRECT_URI,
-
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGODB_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  googleRedirectURI: process.env.GOOGLE_REDIRECT_URI,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID
 };
